57cdabc21754ec85b0c90a2fce7a664598e4b9f9
saidgama08 <saidgama08@gmail.com>
2025-04-12 23:34:36 +0000

    fix5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://SaidGama.github.io/polyvac-web',
-  base: '/polyvac-web/',
+  site: 'https://saidgama.github.io/polyvac-web/',
+  base: 'polyvac-web',
   vite: {
     plugins: [tailwindcss()]
   }
