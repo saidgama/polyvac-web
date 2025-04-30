c41b97a6c642d25a49cd845a67f5781f562c3f88
Said Gama <saidgama08@gmail.com>
2025-04-30 13:39:04 -0600

    test2

--- src/data/productos.js
+++ src/data/productos.js
@@ -27,13 +27,111 @@ export default [
     imagenes: [
       "../assets/cubeta.png"
     ],
-    fichaTecnica: "/pdfs/ficha-plastisol.pdf",
+    fichaTecnica: "https://drive.google.com/file/d/1HtOPaKIEnDW-7xYJIQcXiG29iGs7HTmJ/view?usp=drive_link",
     presentaciones: ["1kg", "4kg", "20kg"],
     codigoColor: "PLAST",
     colores: [
       { nombre: "Blanco", hex: "#fff" },
       { nombre: "Negro", hex: "#222" },
       { nombre: "Azul", hex: "#3490dc" }
+    ]
+  },
+  {
+    id: "ahulado-ao-ht",
+    titulo: "FICHA AHULADO AO HT",
+    categoria: "Dummy",
+    descripcion: "Producto dummy generado para ficha-ahulado-ao-ht.pdf.",
+    imagenes: ["../assets/cubeta.png"],
+    fichaTecnica: "https://drive.google.com/file/d/14Q5F7EgiNmSxrfd4oZmAxwemMaOLd6hS/view?usp=drive_link",
+    presentaciones: ["1kg", "4kg", "20kg"],
+    codigoColor: "DUMMY",
+    colores: [
+      { nombre: "Blanco", hex: "#fff" },
+      { nombre: "Negro", hex: "#222" }
+    ]
+  },
+  {
+    id: "plastisol-vm-ht",
+    titulo: "FICHA PLASTISOL VM HT",
+    categoria: "Dummy",
+    descripcion: "Producto dummy generado para ficha-plastisol-vm-ht.pdf.",
+    imagenes: ["../assets/cubeta.png"],
+    fichaTecnica: "https://drive.google.com/file/d/1HtOPaKIEnDW-7xYJIQcXiG29iGs7HTmJ/view?usp=drive_link",
+    presentaciones: ["1kg", "4kg", "20kg"],
+    codigoColor: "DUMMY",
+    colores: [
+      { nombre: "Blanco", hex: "#fff" },
+      { nombre: "Negro", hex: "#222" }
+    ]
+  },
+  {
+    id: "plastivac-vs-ht",
+    titulo: "FICHA PLASTIVAC VS HT",
+    categoria: "Dummy",
+    descripcion: "Producto dummy generado para ficha-plastivac-vs-ht.pdf.",
+    imagenes: ["../assets/cubeta.png"],
+    fichaTecnica: "https://drive.google.com/file/d/1AQJ5GwGTnVHLf7GlHwnQAMT8l47LPtm2/view?usp=drive_link",
+    presentaciones: ["1kg", "4kg", "20kg"],
+    codigoColor: "DUMMY",
+    colores: [
+      { nombre: "Blanco", hex: "#fff" },
+      { nombre: "Negro", hex: "#222" }
+    ]
+  },
+  {
+    id: "polyprint-ht",
+    titulo: "FICHA POLYPRINT HT",
+    categoria: "Dummy",
+    descripcion: "Producto dummy generado para ficha-polyprint-ht.pdf.",
+    imagenes: ["../assets/cubeta.png"],
+    fichaTecnica: "https://drive.google.com/file/d/12Jow_jkzDVmWxuLLZlom3C-0w62Bc3G6/view?usp=drive_link",
+    presentaciones: ["1kg", "4kg", "20kg"],
+    codigoColor: "DUMMY",
+    colores: [
+      { nombre: "Blanco", hex: "#fff" },
+      { nombre: "Negro", hex: "#222" }
+    ]
+  },
+  {
+    id: "polytransfer-ht",
+    titulo: "FICHA POLYTRANSFER HT",
+    categoria: "Dummy",
+    descripcion: "Producto dummy generado para ficha-polytransfer-ht.pdf.",
+    imagenes: ["../assets/cubeta.png"],
+    fichaTecnica: "https://drive.google.com/file/d/1m0k0Z3o1Xc7TIPantMWwo8h5nKks5bcM/view?usp=drive_link",
+    presentaciones: ["1kg", "4kg", "20kg"],
+    codigoColor: "DUMMY",
+    colores: [
+      { nombre: "Blanco", hex: "#fff" },
+      { nombre: "Negro", hex: "#222" }
+    ]
+  },
+  {
+    id: "sublisol-ht",
+    titulo: "FICHA SUBLISOL HT",
+    categoria: "Dummy",
+    descripcion: "Producto dummy generado para ficha-sublisol-ht.pdf.",
+    imagenes: ["../assets/cubeta.png"],
+    fichaTecnica: "https://drive.google.com/file/d/1P7h3hJF_WMePJLK3PvN5CNM-PNjTvjq5/view?usp=drive_link",
+    presentaciones: ["1kg", "4kg", "20kg"],
+    codigoColor: "DUMMY",
+    colores: [
+      { nombre: "Blanco", hex: "#fff" },
+      { nombre: "Negro", hex: "#222" }
+    ]
+  },
+  {
+    id: "sublivac-ht",
+    titulo: "FICHA SUBLIVAC HT",
+    categoria: "Dummy",
+    descripcion: "Producto dummy generado para ficha-sublivac-ht.pdf.",
+    imagenes: ["../assets/cubeta.png"],
+    fichaTecnica: "https://drive.google.com/file/d/1RwW4JnHgGQo3hhmJWyvaGqh7RGaCRhr-/view?usp=drive_link",
+    presentaciones: ["1kg", "4kg", "20kg"],
+    codigoColor: "DUMMY",
+    colores: [
+      { nombre: "Blanco", hex: "#fff" },
+      { nombre: "Negro", hex: "#222" }
     ]
   }
 ];
